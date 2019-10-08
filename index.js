5e053446592da15a8eb5c6c045d82b069bf35724
Enrique Avello Díaz <uo258119@uniovi.es>
2019-10-08 22:50:41 +0200

    Simple changes

--- index.js
+++ index.js
@@ -6,5 +6,7 @@ app.get('/',(req,res,next)=>{
     res.send('Hello World')
 })
 
-app.listen(5000)
+app.listen(5000) // 3000, 5000,8000,8080
+
+// Simplemente imprime un HelloWorld en localhost 5000
 
