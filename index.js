518011eaf3bf63888f20d6e5893dbb42e653c36f
Enrique Avello Díaz <uo258119@uniovi.es>
2019-10-08 23:09:32 +0200

    Added a JSON response and starting to use the mustache template engine
    with hoggan middleware

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
+app.set('views',path.join(__dirname,'views'))
+app.set('view engine','mustache')
+app.engine('mustache',require('hogan-middleware').__express)
 
 app.get('/',(req,res,next)=>{
     res.send('Hello World')
 })
 
+app.get('/json',(req,res,next)=>{
+    const data= {
+        greeting: 'Hello'
+    }
+    res.json(data)
+})
+
+app.get('/home',(req,res,next)=>{
+    res.render('home',null)
+})
+
 app.listen(5000) // 3000, 5000,8000,8080
 
-// Simplemente imprime un HelloWorld en localhost 5000
+
 
